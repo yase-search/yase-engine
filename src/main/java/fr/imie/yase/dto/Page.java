f7f12cffe6d865770c5662f647f1c71f407e9c4c
BlackYoup <a.lefebvre@outlook.fr>
2016-04-15 10:46:26 +0200

    DTO page: add attributes

--- src/main/java/fr/imie/yase/dto/Page.java
+++ src/main/java/fr/imie/yase/dto/Page.java
@@ -1,5 +1,6 @@
 package fr.imie.yase.dto;
 
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -14,20 +15,32 @@ public class Page {
 	private String url;
 	private Integer id;
 	private String content;
-		
+	private Date crawl_date;
+	private Integer size;
+	private Integer load_time;
+	private String locale;
+	private WebSite website;
+
 	/**
 	 * Construct
 	 * @param title String
 	 * @param text String 
 	 * @param description String
 	 */
-	public Page(Integer id, String title, List<Keywords> keywords, String description, String url, String content) {
+	public Page(Integer id, String title, List<Keywords> keywords,
+			String description, String url, String content,
+			Date crawl_date, Integer size, Integer load_time,
+			String locale) {
 		this.id = id;
 		this.title = title;
 		this.keywords = keywords;
 		this.description = description;
 		this.url = url;
 		this.content = content;
+		this.crawl_date = crawl_date;
+		this.size = size;
+		this.load_time = load_time;
+		this.locale = locale;
 	}
 
 	/**
@@ -84,6 +97,105 @@ public class Page {
 	 */
 	public void setUrl(String url) {
 		this.url = url;
+	}
+	
+	public void setContent(String content){
+		this.content = content;
+	}
+	
+	public String getContent(){
+		return this.content;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the crawl_date
+	 */
+	public Date getCrawl_date() {
+		return crawl_date;
+	}
+
+	/**
+	 * @param crawl_date the crawl_date to set
+	 */
+	public void setCrawl_date(Date crawl_date) {
+		this.crawl_date = crawl_date;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public Integer getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(Integer size) {
+		this.size = size;
+	}
+
+	/**
+	 * @return the load_time
+	 */
+	public Integer getLoad_time() {
+		return load_time;
+	}
+
+	/**
+	 * @param load_time the load_time to set
+	 */
+	public void setLoad_time(Integer load_time) {
+		this.load_time = load_time;
+	}
+
+	/**
+	 * @return the locale
+	 */
+	public String getLocale() {
+		return locale;
+	}
+
+	/**
+	 * @param locale the locale to set
+	 */
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+
+	/**
+	 * @param keywords the keywords to set
+	 */
+	public void setKeywords(List<Keywords> keywords) {
+		this.keywords = keywords;
+	}
+
+	/**
+	 * @return the website
+	 */
+	public WebSite getWebsite() {
+		return website;
+	}
+
+	/**
+	 * @param website the website to set
+	 */
+	public void setWebsite(WebSite website) {
+		this.website = website;
 	}
 	
 }
