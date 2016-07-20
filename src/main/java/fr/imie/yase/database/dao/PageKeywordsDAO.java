29d0912bba70e56dc042def20cd507cb4aa2b254
BlackYoup <a.lefebvre@outlook.fr>
2016-07-20 14:30:15 +0200

    indent

--- src/main/java/fr/imie/yase/database/dao/PageKeywordsDAO.java
+++ src/main/java/fr/imie/yase/database/dao/PageKeywordsDAO.java
@@ -15,90 +15,90 @@ import fr.imie.yase.dto.Page;
 import fr.imie.yase.dto.PageKeywords;
 
 public class PageKeywordsDAO implements DAO<PageKeywords> {
-	
-	// La technique à Erwan pour qu'il soit content et qu'il sourit à ses collègues et à la vie en général
-	private static final String SELECT_TABLE = "SELECT * FROM pages_words where idpage = ? AND idword = ?;";
-	private static final String SELECT_ALL_WORDS_FROM_PAGE =
-			"SELECT w.id, w.text FROM pages_words pw INNER JOIN words w ON w.id = pw.idword WHERE pw.idpage = ?";
-	private static final String INSERT_TABLE = "INSERT INTO pages_words (idpage,idword,strength) VALUES (?,?,?);";
-	private static final String DELETE_TABLE = "";
 
-	private static final String ATT_IDPAGE = "idpage";
-	private static final String ATT_IDKEYWORD = "idword";
-	private static final String ATT_STRENGTH = "strength";
+    // La technique à Erwan pour qu'il soit content et qu'il sourit à ses collègues et à la vie en général
+    private static final String SELECT_TABLE = "SELECT * FROM pages_words where idpage = ? AND idword = ?;";
+    private static final String SELECT_ALL_WORDS_FROM_PAGE =
+            "SELECT w.id, w.text FROM pages_words pw INNER JOIN words w ON w.id = pw.idword WHERE pw.idpage = ?";
+    private static final String INSERT_TABLE = "INSERT INTO pages_words (idpage,idword,strength) VALUES (?,?,?);";
+    private static final String DELETE_TABLE = "";
 
-	public PageKeywords get(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private static final String ATT_IDPAGE = "idpage";
+    private static final String ATT_IDKEYWORD = "idword";
+    private static final String ATT_STRENGTH = "strength";
 
-	public List<PageKeywords> find(Object param) throws SQLException {
-		List<PageKeywords> listPageKeywords = new ArrayList<PageKeywords>();
-		PreparedStatement preparedStatement = preparedStatementTwoIds((PageKeywords) param);
-		ResultSet result = preparedStatement.executeQuery();
-		
-		// Si la requete est différente de null, on ajoute le PageKeywords à la liste.
-		if (!result.wasNull()) {
-			while (result.next()) {              
-			    PageKeywords objectKeywords = new PageKeywords(result.getInt(ATT_IDPAGE), result.getInt(ATT_IDKEYWORD), result.getInt(ATT_STRENGTH));
-			    listPageKeywords.add(objectKeywords);
-			}
-		}
-		return listPageKeywords;
-	}
-	
-	public boolean delete(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public PageKeywords get(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public PageKeywords update(PageKeywords entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<PageKeywords> find(Object param) throws SQLException {
+        List<PageKeywords> listPageKeywords = new ArrayList<PageKeywords>();
+        PreparedStatement preparedStatement = preparedStatementTwoIds((PageKeywords) param);
+        ResultSet result = preparedStatement.executeQuery();
 
-	public PageKeywords create(PageKeywords entity) throws SQLException {
-		Connection  connection = DBConnector.getInstance();
-		PreparedStatement preparedStatement = connection.prepareStatement(INSERT_TABLE, Statement.RETURN_GENERATED_KEYS);
-		preparedStatement.setInt(1, entity.getIdPage());
-		preparedStatement.setInt(2, entity.getIdKeyword());
-		preparedStatement.setInt(3, entity.getStrengh());
+        // Si la requete est différente de null, on ajoute le PageKeywords à la liste.
+        if (!result.wasNull()) {
+            while (result.next()) {
+                PageKeywords objectKeywords = new PageKeywords(result.getInt(ATT_IDPAGE), result.getInt(ATT_IDKEYWORD), result.getInt(ATT_STRENGTH));
+                listPageKeywords.add(objectKeywords);
+            }
+        }
+        return listPageKeywords;
+    }
 
-		preparedStatement.execute();
-		return entity;
-	}
-	
-	/** 
-	 * Permet de préparer l'objet preparedStatement pour une requête find
-	 * @param pageKeywords PageKeywords
-	 * @return PreparedStatement PreparedStatement
-	 * @throws SQLException SQLException
-	 */
-	public PreparedStatement preparedStatementTwoIds(PageKeywords pageKeywords) throws SQLException {
-		Connection connection = DBConnector.getInstance();
-		PreparedStatement preparedStatement = connection.prepareStatement(SELECT_TABLE);
-		preparedStatement.setInt(1, (int) pageKeywords.getIdPage());
-		preparedStatement.setInt(2, (int) pageKeywords.getIdKeyword());
-		return preparedStatement;
-	}
+    public boolean delete(int id) throws SQLException {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	public List<Keywords> findAllKeywordsFromPage(Page page) throws SQLException{
-		Connection connection = DBConnector.getInstance();
-		PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_WORDS_FROM_PAGE);
-		preparedStatement.setInt(1, page.getId());
-		ResultSet result = preparedStatement.executeQuery();
+    public PageKeywords update(PageKeywords entity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-		List<Keywords> keywords = new ArrayList<Keywords>();
+    public PageKeywords create(PageKeywords entity) throws SQLException {
+        Connection  connection = DBConnector.getInstance();
+        PreparedStatement preparedStatement = connection.prepareStatement(INSERT_TABLE, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setInt(1, entity.getIdPage());
+        preparedStatement.setInt(2, entity.getIdKeyword());
+        preparedStatement.setInt(3, entity.getStrengh());
 
-		if(!result.wasNull()){
-			while(result.next()){
-				Keywords keyword = new Keywords();
+        preparedStatement.execute();
+        return entity;
+    }
+
+    /**
+     * Permet de préparer l'objet preparedStatement pour une requête find
+     * @param pageKeywords PageKeywords
+     * @return PreparedStatement PreparedStatement
+     * @throws SQLException SQLException
+     */
+    public PreparedStatement preparedStatementTwoIds(PageKeywords pageKeywords) throws SQLException {
+        Connection connection = DBConnector.getInstance();
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_TABLE);
+        preparedStatement.setInt(1, (int) pageKeywords.getIdPage());
+        preparedStatement.setInt(2, (int) pageKeywords.getIdKeyword());
+        return preparedStatement;
+    }
+
+    public List<Keywords> findAllKeywordsFromPage(Page page) throws SQLException{
+        Connection connection = DBConnector.getInstance();
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_WORDS_FROM_PAGE);
+        preparedStatement.setInt(1, page.getId());
+        ResultSet result = preparedStatement.executeQuery();
+
+        List<Keywords> keywords = new ArrayList<Keywords>();
+
+        if(!result.wasNull()){
+            while(result.next()){
+                Keywords keyword = new Keywords();
                 keyword.setValue(result.getString("text"));
-				keyword.setId(result.getInt("id"));
-				keywords.add(keyword);
-			}
-		}
+                keyword.setId(result.getInt("id"));
+                keywords.add(keyword);
+            }
+        }
 
-		return keywords;
-	}
+        return keywords;
+    }
 }
