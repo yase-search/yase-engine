8a692ca55ac669190d7ffdee7bb9f4e2dc8f3a52
BlackYoup <a.lefebvre@outlook.fr>
2016-08-26 12:33:16 +0200

    Use cache words instead of requesting database

--- src/main/java/fr/imie/yase/Application.java
+++ src/main/java/fr/imie/yase/Application.java
@@ -2,6 +2,8 @@ package fr.imie.yase;
 
 import fr.imie.yase.crawler.CrawlerDBManager;
 import fr.imie.yase.crawler.MainCrawler;
+import fr.imie.yase.database.dao.KeywordsDAO;
+import fr.imie.yase.dto.Keywords;
 
 /**
  * Permet de lancer l'application.
@@ -15,6 +17,7 @@ public class Application extends Thread{
 
 		// Instanciate before crawler starts
         new CrawlerDBManager();
+        KeywordsDAO.populateWords();
         try {
             System.out.println("Start application YASE in thread.");
             new MainCrawler();
--- src/main/java/fr/imie/yase/crawler/CrawlerDBManager.java
+++ src/main/java/fr/imie/yase/crawler/CrawlerDBManager.java
@@ -12,62 +12,11 @@ import java.util.Map;
 public class CrawlerDBManager {
     private static CrawlerDBManager _instance;
 
-    private Map<String, Integer> keywords;
-
     public static CrawlerDBManager getInstance(){
         if(_instance == null){
             _instance = new CrawlerDBManager();
         }
 
         return _instance;
-    }
-
-    public CrawlerDBManager(){
-        this.populateWords();
-        System.out.println("Number of words in database: " + this.keywords.size());
-    }
-
-    private void populateWords(){
-        KeywordsDAO keywordsDAO = new KeywordsDAO();
-        try {
-            this.keywords = keywordsDAO.findAllKeywords();
-        } catch (SQLException e){
-            System.out.println("Couldn't get all words from database");
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    public Map<String, Integer> getKeywords(){
-        return this.keywords;
-    }
-
-    private Keywords createKeyword(String keyword){
-        KeywordsDAO keywordsDAO = new KeywordsDAO();
-        Keywords entity = new Keywords();
-        entity.setValue(keyword);
-
-        try {
-            Keywords created = keywordsDAO.create(entity);
-
-            if(created.getId() != null) {
-                this.keywords.put(created.getValue(), created.getId());
-            }
-
-            return created;
-        } catch (SQLException e){
-            // If insertion fails, return the entity which has no ID
-            return entity;
-        }
-    }
-
-    public Keywords keywordExists(String keyword){
-        Integer keywordId = this.getKeywords().get(keyword);
-        if(keywordId == null) {
-            return this.createKeyword(keyword);
-        } else{
-            // TODO: order of arguments will be changed
-            return new Keywords(keyword, false, keywordId);
-        }
     }
 }
